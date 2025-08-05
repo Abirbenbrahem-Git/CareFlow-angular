fb4f8633105aea91c94ee3b9f230a1b4b82f9e16
Abir Ben Brahem <benbrahemabir@gmail.com>
2025-08-05 11:58:23 +0100

    modif controle de saisie form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -46,25 +46,35 @@ export class FormComponent implements OnInit {
     this.selectedFile = event.target.files[0];
   }
 
-  onSubmit() {
-    if (this.selectedFile) {
-      const formData = new FormData();
-      for (let key in this.bulletin) {
-        formData.append(key, this.bulletin[key]);
-      }
-      formData.append('fichier', this.selectedFile);
+  fileTouched = false;
 
-      this.bulletinService.createBulletin(formData).subscribe({
-        next: res => {
-          alert("Bulletin envoyé avec succès !");
-        },
-        error: err => {
-          console.error(err);
-          alert("Erreur lors de l'envoi du bulletin");
-        }
-      });
-    }
+onSubmit(form: NgForm) {
+  this.fileTouched = true;
+
+  if (form.invalid || !this.selectedFile) {
+    Object.values(form.controls).forEach(control => control.markAsTouched());
+    return;
   }
+
+  const formData = new FormData();
+  for (let key in this.bulletin) {
+    formData.append(key, this.bulletin[key]);
+  }
+  formData.append('fichier', this.selectedFile);
+
+  this.bulletinService.createBulletin(formData).subscribe({
+    next: res => {
+      alert("Bulletin envoyé avec succès !");
+      form.resetForm();
+      this.selectedFile = null;
+      this.fileTouched = false;
+    },
+    error: err => {
+      console.error(err);
+      alert("Erreur lors de l'envoi du bulletin");
+    }
+  });
+}
 
 onResetForm(form: NgForm) {
     form.resetForm(); 
