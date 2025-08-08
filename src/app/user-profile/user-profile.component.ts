a92862023edd7783bc4b61da299106e8268e40ec
Abir Ben Brahem <benbrahemabir@gmail.com>
2025-08-08 23:51:52 +0100

    update profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,13 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  user: any;
+  user: any = {};
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     const storedUser = localStorage.getItem('currentUser');
     if (storedUser) {
       this.user = JSON.parse(storedUser);
     }
+  }
+
+  onUpdateProfile() {
+    this.http.put(`http://localhost:8080/user/${this.user.iduser}`, this.user)
+      .subscribe({
+        next: (updatedUser: any) => {
+          this.user = updatedUser;
+          localStorage.setItem('currentUser', JSON.stringify(updatedUser));
+          alert('Profil mis à jour avec succès');
+        },
+        error: () => {
+          alert('Erreur lors de la mise à jour du profil');
+        }
+      });
   }
 
 }
